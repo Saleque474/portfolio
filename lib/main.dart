25dcd6abf1a8f7b4d4a591ef451f6253fa02370b
Saleque474 <56844858+Saleque474@users.noreply.github.com>
2021-07-06 05:51:33 +0600

    Uploaded on github

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'short_view/short_view.dart';
+
+void main() {
+  runApp(MyPortfolio());
+}
+
+class MyPortfolio extends StatelessWidget {
+  const MyPortfolio({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      
+    );
+  }
+}
